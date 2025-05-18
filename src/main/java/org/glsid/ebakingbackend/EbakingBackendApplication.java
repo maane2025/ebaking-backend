dbcf8a8797e3ab2ada69ca09b3103fd4f9c7f23d
maane2025 <maane2025@users.noreply.github.com>
2025-05-19 00:40:55 +0100

    Initial commit

---
+++ src/main/java/org/glsid/ebakingbackend/EbakingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.glsid.ebakingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbakingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbakingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/glsid/ebakingbackend/EbakingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.glsid.ebakingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbakingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
